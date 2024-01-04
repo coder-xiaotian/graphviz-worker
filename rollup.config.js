8a1c53e23803b5f73e65ffd18cd39fa87cb65756
xiaotian <wanxiaotian888@gmail.com>
2024-01-04 18:33:30 +0800

    升级依赖

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,19 @@
-import webWorkerLoader from 'rollup-plugin-web-worker-loader';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import terser from '@rollup/plugin-terser';
+import webWorkerLoader from "rollup-plugin-web-worker-loader";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
 
-export default [{
-  input: 'src/index.js',
-  output: {
-    name: "Graphviz",
-    format: 'umd',
-    dir: "lib"
+export default [
+  {
+    input: "src/index.js",
+    output: {
+      name: "Graphviz",
+      format: "umd",
+      dir: "lib",
+    },
+    plugins: [
+      nodeResolve(),
+      webWorkerLoader(),
+      terser()
+    ],
   },
-  plugins: [
-    nodeResolve(),
-    webWorkerLoader(),
-    terser()
-  ]
-}];
+];
